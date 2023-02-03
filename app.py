72fa60bd021eec8ce40728cbea48457961f7c59e
Tok6Flow0 <aaronmopsamuel@gmail.com>
2023-02-03 15:16:03 -0500

    added base code

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+#---------------------------------------
+#LAB START
+#---------------------------------------
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "https://ecse-week3-demo.netlify.app"
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+fake_database = []
+
+@app.get("/todos")
+async def get_all_todos():
+  return fake_database
+
+@app.post("/todos")
+async def create_todo(request: Request):
+  todo = await request.json()
+  fake_database.append(todo)
+  return todo
